b1867487165d8e50ebbe98d75131cea13b142cda
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2023-09-17 18:37:44 +0500

    Slider setting

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,12 +1,51 @@
 import React from "react";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-import "swiper/css"; // built in css of swiper
+
 import "./Residencies.css";
 import data from "../../utils/slider.json";
-import { sliderSettings } from "../../utils/common";
-// import ' swiper/css/Autoplay'  
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Residencies = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+    
+  };
+
   return (
     <section className="r-wrapper" id="residencies">
       <div className="paddings innerWidth r-container">
@@ -14,32 +53,27 @@ const Residencies = () => {
           <span className="orangeText">Best Choices</span>
           <span className="primaryText">Popular Residencies</span>
         </div>
-        {/* <SliderButtons /> */}
-        <Swiper {...sliderSettings}
-         autoplay = {true}
-        // modules={[Autoplayer]}
-        
-        >
-         
+
+        <Slider {...settings}>
           {data.map((card, i) => (
-            <SwiperSlide key={i}>
+            <Slider key={i}>
               <div className="flexColStart r-card">
                 <img src={card.image} alt="" />
-
-                <span className="secondaryText r-price">
-                  <span className="dollarsigin">$</span>
-                  <span>{card.price}</span>
-                </span>
-                <span className="primaryText">{card.name}</span>
-                <span className="secondaryText">{card.detail}</span>
+                <div className="text">
+                  <span className="secondaryText r-price">
+                    <span className="dollarsigin">$</span>
+                    <span>{card.price}</span>
+                  </span>
+                  <span className="primaryText">{card.name}</span>
+                  <span className="secondaryText">{card.detail}</span>
+                </div>
               </div>
-            </SwiperSlide>
+            </Slider>
           ))}
-        </Swiper>
+        </Slider>
       </div>
     </section>
   );
 };
 
 export default Residencies;
-
